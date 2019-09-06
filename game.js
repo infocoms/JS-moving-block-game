1af9d7d43725677ccd6016c273cb75b3111f5ed4
infocoms <kevincomyn@gmail.com>
2019-09-06 17:58:12 +0200

    added a lives counter with some heart icons & when lives are gone reset the game

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 
 var stage = document.getElementById('myCanvas'),
-    ctx = stage.getContext('2d');
+    ctx = stage.getContext('2d'),
+    life = 3;
 
 
 
-
+console.log(life);
 
 
 var char = new Image();
@@ -24,31 +25,56 @@ enemy.src = "img/meowth.svg";
     hi = 70;
 
 
+var heart = new Image();
+heart.src = "img/life.svg";
+h1x = 100;
+
+
+yh = 10;
+wh = 30;
+hh = 30;
+
+
+
+
+
+
 
 
 function createChar(x,y,w,h) {
     ctx.drawImage(char, x, y , w,h);
+    ctx.font = "30px Arial";
+    ctx.fillText("Life: "+ life,10,30);
+
 }
 
 function createEnemy(xi,yi,wi,hi) {
     ctx.drawImage(enemy, xi, yi , wi, hi);
 }
 
+function gameOver() {
+    ctx.clearRect(0,0, stage.width, stage.height);
+    location.reload()
 
 
+}
 
+
+ctx.drawImage(heart, 100, yh , wh, hh);
+ctx.drawImage(heart, 140, yh , wh, hh);
+ctx.drawImage(heart, 180, yh , wh, hh);
 createChar(x,y,w,h);
 createEnemy(xi,yi,wi,hi);
 
 
 
+/*key-controls*/
 
 window.onkeydown = function(event) {
     var keyPr = event.keyCode;
 
     if(keyPr === 39 && x<=1220){
         x = x+10;
-
     }
     else if(keyPr === 37 && x>8){
         x = x-10;
@@ -63,36 +89,42 @@ window.onkeydown = function(event) {
     }
 
 
-    if ((y + h) < (yi) || (y) > (yi + hi) || (x + w) < (xi) || (x) > (xi+wi)) {
-        console.log("Not colliding");
-
-
-
-    }else {
-        alert("you are dead madafakka");
-
-    }
-
-
-
-
-    /*( ( char.y + char.height ) < ( enemy.y ) ) ||
-    ( char.y > ( enemy.y + enemy.height ) ) ||
-    ( ( char.x + char.width ) < enemy.x ) ||
-    ( char.x > ( enemy.x + enemy.width ) )*/
-
-
-
-
-
-
-
-
-
 
     ctx.clearRect(0,0, 1500, 900);
     createChar(x,y,w,h);
     createEnemy(xi,yi,wi,hi);
+
+
+    if ((y + h) < (yi) ||
+        (y) > (yi + hi) ||
+        (x + w) < (xi) ||
+        (x) > (xi+wi)) {
+        console.log("Not colliding");
+        if (life === 3){
+            ctx.drawImage(heart, 100, yh , wh, hh);
+            ctx.drawImage(heart, 140, yh , wh, hh);
+            ctx.drawImage(heart, 180, yh , wh, hh);
+        }
+        else if(life === 2){
+            ctx.drawImage(heart, 100, yh , wh, hh);
+            ctx.drawImage(heart, 140, yh , wh, hh);
+        }
+        else if(life === 1){
+            ctx.drawImage(heart, 100, yh , wh, hh);
+        }
+
+
+    }else {
+
+        life = life - 1;
+        console.log(life);
+
+
+        if(life <= 0){
+            gameOver();
+            alert("you Died!")
+        }
+    }
 
 
 
