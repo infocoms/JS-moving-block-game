9c16021dc5f6f160c8727c154134dcd60b0df3f3
infocoms <kevincomyn@gmail.com>
2019-09-06 19:26:05 +0200

    added a food object and it adds a life when touched, max health is 5. improved the heart removal animation when touched by enemy

--- game.js
+++ game.js
@@ -1,132 +1,164 @@
-
 var stage = document.getElementById('myCanvas'),
     ctx = stage.getContext('2d'),
     life = 3;
-
-
-
 console.log(life);
+/*object to load on load*/
 
 
 var char = new Image();
 char.src = "img/pikachu.svg";
-    x = 200;
-    y = 500;
-    w = 70;
-    h = 70;
-
+x = 200;
+y = 500;
+w = 70;
+h = 70;
 
 
 var enemy = new Image();
 enemy.src = "img/meowth.svg";
-    xi = 100;
-    yi = 100;
-    wi = 70;
-    hi = 70;
+xi = 100;
+yi = 100;
+wi = 70;
+hi = 70;
 
 
 var heart = new Image();
 heart.src = "img/life.svg";
-h1x = 100;
-
-
 yh = 10;
 wh = 30;
 hh = 30;
 
 
+var food = new Image();
+food.src = "img/food.svg";
+fx = 800;
+fy = 500;
+fw = 40;
+fh = 40;
 
 
+Start();
 
-
-
-
-function createChar(x,y,w,h) {
-    ctx.drawImage(char, x, y , w,h);
-    ctx.font = "30px Arial";
-    ctx.fillText("Life: "+ life,10,30);
+function Start() {
+    hearts();
+    createChar(x, y, w, h);
+    createEnemy(xi, yi, wi, hi);
 
 }
 
-function createEnemy(xi,yi,wi,hi) {
-    ctx.drawImage(enemy, xi, yi , wi, hi);
+
+function createChar(x, y, w, h) {
+    ctx.drawImage(char, x, y, w, h);
+    ctx.font = "30px Arial";
+    ctx.fillText("Life: " + life, 10, 30);
+
+}
+
+function createEnemy(xi, yi, wi, hi) {
+    ctx.drawImage(enemy, xi, yi, wi, hi);
 }
 
 function gameOver() {
-    ctx.clearRect(0,0, stage.width, stage.height);
+    ctx.clearRect(0, 0, stage.width, stage.height);
     location.reload()
 
 
 }
 
-
-ctx.drawImage(heart, 100, yh , wh, hh);
-ctx.drawImage(heart, 140, yh , wh, hh);
-ctx.drawImage(heart, 180, yh , wh, hh);
-createChar(x,y,w,h);
-createEnemy(xi,yi,wi,hi);
-
+function hearts() {
+    ctx.drawImage(heart, 100, yh, wh, hh);
+    ctx.drawImage(heart, 140, yh, wh, hh);
+    ctx.drawImage(heart, 180, yh, wh, hh);
+    ctx.drawImage(food, fx, fy, fw, fh);
+}
 
 
 /*key-controls*/
 
-window.onkeydown = function(event) {
+window.onkeydown = function (event) {
     var keyPr = event.keyCode;
 
-    if(keyPr === 39 && x<=1220){
-        x = x+10;
+    if (keyPr === 39 && x <= 1220) {
+        x = x + 10;
+    } else if (keyPr === 37 && x > 8) {
+        x = x - 10;
+
+    } else if (keyPr === 38 && y > 8) {
+        y = y - 10;
+
+    } else if (keyPr === 40 && y <= 620) {
+        y = y + 10;
     }
-    else if(keyPr === 37 && x>8){
-        x = x-10;
-
-    }
-    else if(keyPr === 38 && y>8) {
-        y = y-10;
-
-    }
-    else if(keyPr === 40 && y<=620){
-        y = y+10;
-    }
-
-
-
-    ctx.clearRect(0,0, 1500, 900);
-    createChar(x,y,w,h);
-    createEnemy(xi,yi,wi,hi);
 
 
     if ((y + h) < (yi) ||
         (y) > (yi + hi) ||
         (x + w) < (xi) ||
-        (x) > (xi+wi)) {
-        console.log("Not colliding");
-        if (life === 3){
-            ctx.drawImage(heart, 100, yh , wh, hh);
-            ctx.drawImage(heart, 140, yh , wh, hh);
-            ctx.drawImage(heart, 180, yh , wh, hh);
-        }
-        else if(life === 2){
-            ctx.drawImage(heart, 100, yh , wh, hh);
-            ctx.drawImage(heart, 140, yh , wh, hh);
-        }
-        else if(life === 1){
-            ctx.drawImage(heart, 100, yh , wh, hh);
-        }
+        (x) > (xi + wi)) {
 
-
-    }else {
+    } else {
 
         life = life - 1;
         console.log(life);
 
 
-        if(life <= 0){
+        if (life <= 0) {
             gameOver();
             alert("you Died!")
         }
     }
 
 
+    if ((y + h) < (fy) ||
+        (y) > (fy + fh) ||
+        (x + w) < (fx) ||
+        (x) > (fx + fw)) {
+        console.log("No food");
+
+    } else {
+
+        console.log("Catch food");
+        life = life + 1;
+        console.log(life);
+
+    }
+
+
+    ctx.clearRect(0, 0, 1500, 900);
+    createChar(x, y, w, h);
+    createEnemy(xi, yi, wi, hi);
+    ctx.drawImage(food, fx, fy, fw, fh);
+
+    /*heart icons corresponds to amount of lives */
+    if (life === 5) {
+        ctx.drawImage(heart, 100, yh, wh, hh);
+        ctx.drawImage(heart, 140, yh, wh, hh);
+        ctx.drawImage(heart, 180, yh, wh, hh);
+        ctx.drawImage(heart, 220, yh, wh, hh);
+        ctx.drawImage(heart, 260, yh, wh, hh);
+    } else if (life === 4) {
+        ctx.drawImage(heart, 100, yh, wh, hh);
+        ctx.drawImage(heart, 140, yh, wh, hh);
+        ctx.drawImage(heart, 180, yh, wh, hh);
+        ctx.drawImage(heart, 220, yh, wh, hh);
+    } else if (life === 3) {
+        ctx.drawImage(heart, 100, yh, wh, hh);
+        ctx.drawImage(heart, 140, yh, wh, hh);
+        ctx.drawImage(heart, 180, yh, wh, hh);
+    } else if (life === 2) {
+        ctx.drawImage(heart, 100, yh, wh, hh);
+        ctx.drawImage(heart, 140, yh, wh, hh);
+    } else if (life === 1) {
+        ctx.drawImage(heart, 100, yh, wh, hh);
+    }
+
+
+    /*MAX LIFE*/
+    if (life >= 5) {
+        life = 5;
+        console.log("stop spawning food");
+        fx = fx - 1000;
+
+    }
 
 };
 
